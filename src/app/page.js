f7c30b30b04b21505eb18c651d56605c639fe77e
Theresa <theresa@Mac.attlocal.net>
2025-02-24 09:08:56 -0800

    redirect to login if logged out

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+"use client";
 import Link from "next/link";
+import { UserButton, useUser } from '@stackframe/stack';
 
 export default function Home() {
-  return (
+    useUser({ or: 'redirect' });
+    return (
       <div>
+          <nav>
+            <UserButton/>
+        </nav>
         <Link href="/add">+</Link>
       </div>
   );
