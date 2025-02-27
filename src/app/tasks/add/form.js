687e68de7cf5b4330569e13580cbb4c290220577
Theresa <theresa@Mac.attlocal.net>
2025-02-26 18:10:12 -0800

    adding fields to add form

--- src/app/tasks/add/form.js
+++ src/app/tasks/add/form.js
@@ -2,11 +2,20 @@
 import supabase from "../../utils/db";
 
 export async function addTask(formData) {
+    console.log(formData);
+
     const { data, error } = await supabase.from('tasks').insert({
         task_name: formData.get("task_name"),
         user_id: formData.get("user_id"),
         task_id: formData.get("task_id"),
-        task_isActive: 1
+        task_isActive: true,
+        task_frequency_cadence: formData.get("task_frequency_cadence"),
+        task_frequency_interval: formData.get("task_frequency_interval"),
+        task_image: formData.get("task_image"),
+        task_repeat: formData.get("task_repeat"),
+        task_notes: formData.get("task_notes"),
+        task_folder: formData.get("task_folder"),
+        task_parent: formData.get("task_parent")
     });
     if (error) {
         console.error(error);
