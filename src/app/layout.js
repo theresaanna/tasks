6ad9a0b4c4d2cb07834858f79b9ad6747cc77476
Theresa <theresa@Mac.attlocal.net>
2025-02-28 18:00:44 -0800

    adding Sniglet font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,18 @@
 "use server"
-import { Geist, Geist_Mono } from "next/font/google";
+import { Sniglet } from "next/font/google";
 import { StackProvider, StackTheme } from "@stackframe/stack";
 import { stackServerApp } from "../stack";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const sniglet = Sniglet({
+    weight: '400'
 });
 
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${sniglet.variable}`}
       >
       <StackProvider app={stackServerApp}><StackTheme>
         {children}
