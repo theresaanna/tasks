9159a6cd544fc9bdcb92d5b89c49c9261344883a
Theresa <theresa@Mac.attlocal.net>
2025-02-28 17:27:52 -0800

    pulling in components and data for basic dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,54 @@ import React from "react";
 import Link from "next/link";
 import { UserButton } from '@stackframe/stack';
 import { stackServerApp } from "@/stack";
+import { createSupabaseClient } from "@/app/utils/supabase/server";
+import AddTask from "./tasks/add/page";
 
 export default async function Home() {
     const user = await stackServerApp.getUser({ or: 'redirect' });
+    const supabase = await createSupabaseClient();
+
+    const getNewestTasks = async (user) => {
+        const {data, error} =
+            await supabase
+                .from('tasks')
+                .select('*')
+                .eq('user_id', user.id)
+                .eq('task_isActive', true);
+        if (error) {
+            console.error(error);
+        } else {
+            const sortedDataDesc = data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+            return sortedDataDesc;
+        }
+    }
+
+    const recentTasks = await getNewestTasks(user);
 
     return (
       <div>
           <nav>
             <UserButton/>
-            Hi, {user?.displayName || 'friend'}!</nav>
-        <Link href="/tasks/add">+</Link>
-        <Link href={"tasks/view"}>View all tasks</Link>
+            Hi, {user?.displayName || 'friend'}!
+          </nav>
+
+          <div className="newest-tasks">
+            <h2>Newest Tasks:</h2>
+              <ul>
+                  {recentTasks?.map((task) => (
+                      <li key={task.task_id}>{task.task_name}</li>
+                  ))}
+              </ul>
+          </div>
+
+          <div className="manage-folders">
+              <h2>Manage Folders</h2>
+          </div>
+
+          <div className="add-task">
+              <h2>Add Task</h2>
+              <AddTask />
+          </div>
       </div>
   );
 }
