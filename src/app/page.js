bea1afd8379b22415a408a347343304f26c07a98
Theresa <theresa@Mac.attlocal.net>
2025-02-28 17:04:20 -0800

    home page is a server component now

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import { UserButton, useUser } from '@stackframe/stack';
+import { UserButton } from '@stackframe/stack';
+import { stackServerApp } from "@/stack";
 
-export default function Home() {
-    const user = useUser({ or: 'redirect' });
+export default async function Home() {
+    const user = await stackServerApp.getUser({ or: 'redirect' });
 
     return (
       <div>
           <nav>
             <UserButton/>
-            Hi, {user?.displayName || 'friend'}!
-              {user?.id}
-        </nav>
+            Hi, {user?.displayName || 'friend'}!</nav>
         <Link href="/tasks/add">+</Link>
         <Link href={"tasks/view"}>View all tasks</Link>
       </div>
